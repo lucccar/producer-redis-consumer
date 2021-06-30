b519052898039194a94583bc0a317fd75c24b5bb
Lucas Carvalho <lucas.crvlh.s@gmail.com>
2021-06-29 23:57:03 -0300

    adding result customization in process endpoint

--- producer/producer.py
+++ producer/producer.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 from rq import Queue
-import os
 
 from producer.constants import conn
 from producer.base.write import write_txt
@@ -16,14 +15,20 @@ q = Queue(connection=conn)
 
 @app.route("/process", methods = ["POST"])
 def process():
+    try:
+        json = request.json
 
-    json = request.json
+        if json == None:
+            return "Invalid Input", 400
 
-    job = q.enqueue_call(
-        func=write_txt, args=(json,), result_ttl=5000
-    )
+        job = q.enqueue_call(
+            func=write_txt, args=(json,), result_ttl=5000
+        )
 
-    return jsonify(job.get_id()), 200
+        return jsonify(job.get_id()), 200
+    
+    except:
+        abort(500, "Some unknown error occurred'")
 
     
 
